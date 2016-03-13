a227da3fca693d547591b4ab76122683d412537e
apieprz6 <apieprz6@students.d125.org>
2016-03-12 20:41:03 -0600

    Got main functionality done for HubCode, not compiled yet

--- HubCode/HubCode.py
+++ HubCode/HubCode.py
@@ -1,5 +1,10 @@
 def sendCommand(motorvalue):
-    from subprocess import call
-    call(["sudo", "/home/pi/Git/433Utils/RPi_utils/codesend" , motorvalue])
+    if motorvalue = 180:
+        from subprocess import call
+        call(["sudo", "/home/pi/Git/433Utils/RPi_utils/codesend" , "180"])
+    else
+        from subprocess import call
+        call(["sudo", "/home/pi/Git/433Utils/RPi_utils/codesend" , "0"])
 
 sendCommand(180);
+
