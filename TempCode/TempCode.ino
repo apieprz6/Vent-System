d4eb465eecabbd009c2304b5056e263b1a960942
apieprz6 <apieprz6@students.d125.org>
2016-04-12 13:51:51 -0500

    Added tmp36 functionality

--- TempCode/TempCode.ino
+++ TempCode/TempCode.ino
@@ -8,8 +8,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define aref_voltage 3.3
+
 RCSwitch rcSwitch = RCSwitch();
-const int TEMPSENSORPIN = 0;//Analog pin 0
+const int THERMISTORPIN = 0;//Analog pin 0
+const int TEMPPIN=1;
 const int TRANSMITPIN = 10;
 const unsigned long long int INTERVAL = 5000;//This is the refresh timer, it determines 
 //how often temp data gets sent to the pi. Currently set to 5 seconds, 
@@ -18,6 +21,7 @@ unsigned long long int previousMillis = 0;//This might be a problem when time st
 
 void setup() {
   Serial.begin(9600);
+  analogReference(EXTERNAL);
   rcSwitch.enableTransmit(TRANSMITPIN);
 }
 
@@ -25,21 +29,35 @@ void loop() {
   unsigned long long int currentMillis = millis();
   if((currentMillis - previousMillis) >= INTERVAL){
     previousMillis = currentMillis;
-    int reading = analogRead(TEMPSENSORPIN);
-    double celsius = Thermistor(reading);
-    double fahren = celsius * 9 / 5 + 32;
-    Serial.print("Temp = ");
-    Serial.print(fahren);
+//    int reading = analogRead(THERMISTORPIN);
+//    double celsius = Thermistor(reading);
+//    double fahren = celsius * 9 / 5 + 32;
+//    Serial.print("Thermistor Temp = ");
+//    Serial.print(fahren);
+//    Serial.println("F");
+    Serial.print("temp36 temp = ");
+    Serial.print(tmp36());
     Serial.println("F");
-    double tempToSend = fahren * 100;
-    rcSwitch.send((int)tempToSend,24);
+    double tempToSend = tmp36() * 100;
+    rcSwitch.send((int)tempToSend,24);//change to tmp36 plz
   }
 }
 
-double Thermistor(int RawADC) {
-  double Temp;
-  Temp = log(((10240000 / RawADC) - 10000));
-  Temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * Temp * Temp )) * Temp );
-  Temp = Temp - 273.15;
-  return Temp;
+//double Thermistor(int RawADC) {
+//  double Temp;
+//  Temp = log(((10240000 / RawADC) - 10000));
+//  Temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * Temp * Temp )) * Temp );
+//  Temp = Temp - 273.15;
+//  return Temp;
+//}
+
+double tmp36(){
+  int tempReading;
+  tempReading = analogRead(TEMPPIN);
+  double voltage = tempReading * aref_voltage;
+  voltage /= 1024.0;
+  double temperatureC = (voltage - 0.5) * 100 ;
+  double temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
+  return temperatureF;
 }
+
--- VentTempCode/VentTempCode.ino
+++ VentTempCode/VentTempCode.ino
@@ -11,10 +11,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define aref_voltage 3.3
 
 RCSwitch rcSwitch = RCSwitch();
 Servo servo;
 const int TEMPSENSORPIN=0;//analog pin 0
+const int TEMPPIN=1;//analog pin 1 for the temperature sensor
 const int SERVOPIN = 9;
 const int TRANSMITPIN = 10;
 const int RECIEVEPIN = 0; //Actually pin 2
@@ -25,7 +27,8 @@ double fahren = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(9600);  
+  analogReference(EXTERNAL);
   rcSwitch.enableTransmit(TRANSMITPIN);
   rcSwitch.enableReceive(RECIEVEPIN); //pin 2
   servo.attach(SERVOPIN);
@@ -48,13 +51,13 @@ void loop() {
   unsigned long long int currentMillis = millis();
   if((currentMillis - previousMillis) >= INTERVAL){
     previousMillis = currentMillis;
-    int reading = analogRead(TEMPSENSORPIN);
-    double celsius =  Thermistor(reading);
-    fahren = celsius * 9 / 5 + 32; 
+//    int reading = analogRead(TEMPSENSORPIN);
+//    double celsius =  Thermistor(reading);
+//    fahren = celsius * 9 / 5 + 32; 
     Serial.print("Temp = ");
-    Serial.print(fahren);
+    Serial.print(tmp36());
     Serial.println("F");
-    double tempToSend = fahren *100;
+    double tempToSend = tmp36() *100;
     Serial.print("Sending ");
     Serial.println((int)tempToSend);
     rcSwitch.send((int)tempToSend,24);
@@ -64,16 +67,26 @@ void loop() {
 
 int getMotorValue() {
   int value = rcSwitch.getReceivedValue();
-  Serial.print("Received ");
+  Serial.print("RECIEVED ");
   Serial.println(value);
   rcSwitch.resetAvailable();
   return value;
 }
 
-double Thermistor(int RawADC) {
-  double Temp;
-  Temp = log(((10240000 / RawADC) - 10000));
-  Temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * Temp * Temp )) * Temp );
-  Temp = Temp - 273.15;
-  return Temp;
+//double Thermistor(int RawADC) {
+//  double Temp;
+//  Temp = log(((10240000 / RawADC) - 10000));
+//  Temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * Temp * Temp )) * Temp );
+//  Temp = Temp - 273.15;
+//  return Temp;
+//}
+
+double tmp36(){
+  int tempReading;
+  tempReading = analogRead(TEMPPIN);
+  double voltage = tempReading * aref_voltage;
+  voltage /= 1024.0;
+  double temperatureC = (voltage - 0.5) * 100 ;
+  double temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
+  return temperatureF;
 }
